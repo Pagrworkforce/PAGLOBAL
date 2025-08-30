78c15bc6b0a14d389485539d0d8d6a20222544fd
Patrick Marcus <patrickmarcus580@gmail.com>
2025-08-30 19:22:14 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link';
 import { useState } from 'react';
 
 const navLinks = [
+  { href: '/', label: 'Home' },
   { href: '/about', label: 'About' },
   { href: '/platforms', label: 'Platforms' },
   { href: '/contact', label: 'Contact' },
