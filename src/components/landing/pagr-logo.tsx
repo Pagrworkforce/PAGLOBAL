18ea69d00f3f983d566163e59cec397d3a088c18
Patrick Marcus <patrickmarcus580@gmail.com>
2025-10-07 18:44:49 +0000

    pls update and modify it to this new image address 'https://i.imgur.com/

--- src/components/landing/pagr-logo.tsx
+++ src/components/landing/pagr-logo.tsx
@@ -5,7 +5,7 @@ export function PagrLogo({ className }: { className?: string }) {
   return (
     <div className={cn('relative', className)}>
       <Image
-        src="https://i.imgur.com/dT4fH6b.jpeg"
+        src="https://i.imgur.com/Tk9cSEK.jpeg"
         alt="PAGR Logo"
         width={28}
         height={28}
